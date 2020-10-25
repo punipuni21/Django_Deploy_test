f0d96d43bb18d0cfb292403c64fabd6cbf205571
punipuni21 <punihobby@gmail.com>
2020-10-25 10:28:59 +0900

    update setting

--- myapp/myapp/local_setting.py
+++ myapp/myapp/local_setting.py
@@ -3,7 +3,8 @@ import os
 #settings.pyからそのままコピー
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '#eynx8vqyj)5(mmv3d&dujk%@ma2--3m@qm&9tz*0o%mo5zpg('
+# SECRET_KEY = '#eynx8vqyj)5(mmv3d&dujk%@ma2--3m@qm&9tz*0o%mo5zpg('
+# SECRET_KEY = '#eynx8vqyj)5(mmv3d&dujk%@ma2--3m@qm&9tz*0o%mo5zpg('
 
 #settings.pyからそのままコピー
 DATABASES = {
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # ALLOWED_HOSTS = ['*'] #こちらでもokです
