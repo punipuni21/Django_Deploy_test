5fc9a5fbda511596a6feda905b1f4360923e2ade
punipuni21 <punihobby@gmail.com>
2020-10-25 12:34:23 +0900

    update

--- mytest/mytest/settings.py
+++ mytest/mytest/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
